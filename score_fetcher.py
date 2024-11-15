a088061807a1ec26f73c3bd145f91618ebabb228
Hannah Greene <questor96@gmail.com>
2024-11-15 17:41:15 -0600

    fork lookup behavior based on _take param
    intended to allow identical entry points for lookup

--- score_fetcher.py
+++ score_fetcher.py
@@ -15,6 +15,14 @@ pp = pprint.PrettyPrinter(indent=2)
 
 
 async def load_from_url(url, params=None):
+    # fork behavior if _take is set
+    if params.get('_take', None) is not None:
+        return await _load_from_url(url, params)
+    else:
+        return await _load_all_from_url(url, params)
+
+
+async def _load_from_url(url, params=None):
     # TODO: error handling
     if params is not None:
         url = f'{url}?params={json.dumps(params)}'
@@ -26,13 +34,13 @@ async def load_from_url(url, params=None):
     return response.json()
 
 
-async def load_all_from_url(url, params:dict = {}):
+async def _load_all_from_url(url, params:dict = {}):
     data = []
     complete = False
     params['_take'] = 100
     params['_skip'] = 0
     while not complete:
-        data += await load_from_url(url, params)
+        data += await _load_from_url(url, params)
         params['_skip'] += params['_take']
         if len(data) < params['_skip']: complete = True
     return data
@@ -65,7 +73,7 @@ async def load_data_incremental(filepath, base_api_url):
     params = {}
     if previous_search:
         params = {"updated_at": {"gt": str(prev_upd_at)}}
-    new_data = await load_all_from_url(base_api_url, params)
+    new_data = await load_from_url(base_api_url, params)
 
     # merge new data into existing
     ids_to_update = [entry['id'] for entry in new_data]
@@ -101,7 +109,7 @@ async def load_charts():
 # generic score loading
 async def load_scores(params):
     url = 'http://api.smx.573.no/scores'
-    data = await load_all_from_url(url, params)
+    data = await load_from_url(url, params)
     return data
 
 
@@ -113,7 +121,8 @@ async def load_player_scores(
         end:datetime = None,
         chart_ids:list[int] = None,
         sort_field:str = None,
-        order:str = None
+        order:str = None,
+        take:int = None
     ):
     params = {'gamer.username': player_name}
     if start is not None or end is not None:
@@ -123,6 +132,7 @@ async def load_player_scores(
     update_dict_if_not_null(params, 'chart.id', chart_ids)
     update_dict_if_not_null(params, '_sort', sort_field)
     update_dict_if_not_null(params, '_order', order)
+    update_dict_if_not_null(params, '_take', take)
     data = await load_scores(params)
     return data
 
@@ -160,7 +170,10 @@ if __name__ == '__main__':
         print(str(len(charts)) + " charts loaded")
     
 
-    # load event configuration, pull from file eventually
+    # load event configuration
+    #   pull from file eventually
+    start = datetime.date(year=2024, month=11, day=1)
+    end = datetime.date(year=2024, month=11, day=13)
     players = [
         "Thaya",
         "Hamaon"
@@ -170,17 +183,18 @@ if __name__ == '__main__':
         {"title": "Ring the Alarm", "difficulty": 22},
         {"title": "Do My Thing", "difficulty": 20}
     ]
-    start = datetime.date(year=2024, month=11, day=1)
-    end = datetime.date(year=2024, month=11, day=13)
     num_tries_to_count = 3
+    sort = 'created_at'
+    order = 'asc'
 
+
+    # Find chart ids based on event song_info
     # these suck lmao, how do I write them more effectively
     for song in song_info:
         for s in songs:
             if song['title'].casefold() == s['title'].casefold():
                 song['id'] = s['id']
                 break
-
     chart_ids = []
     for song in song_info:
         for chart in charts:
@@ -190,7 +204,6 @@ if __name__ == '__main__':
 
     
     # load score data asynchronously
-    # loop already open?
     coroutines = []
     for player in players:
         for chart_id in chart_ids:
@@ -200,13 +213,14 @@ if __name__ == '__main__':
                     start=start,
                     end=end,
                     chart_ids=chart_id,
-                    sort_field='created_at',
-                    order='asc',
+                    sort_field=sort,
+                    order=order,
+                    take=num_tries_to_count
                 )
             )
     all_results = loop.run_until_complete(asyncio.gather(*coroutines))
     for result in all_results:
-        for score in result[:num_tries_to_count]:
+        for score in result:
             print(' '.join([score['gamer']['username'], score['song']['title'], str(score['score'])]))
 
     # clean up event loop
